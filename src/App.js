bf6154b980489aed00806f7b5dc93880dd1cf5d3
ryo0705s <inspire.sasaki15@gmail.com>
2021-04-28 23:12:02 +0900

    おみくじアプリ更新

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+
 
 const lucky = ['大吉', '中吉', '小吉', '凶']
-const n = Math.floor(Math.random * lucky.length)
+const n = Math.floor(Math.random() * lucky.length)
 
 function App() {
-  const handleClick = () => {
-    console.log(handleClick)
-  }
+  const [state, setState] = useState()
+  const newState = [state, lucky]
+  // const handleClick = () => {
+  // }
+  console.log(newState)
   return (
     <div className="App">
-      <div id="btn" >{lucky[n]}</div>
+      <div id="btn" value={newState} onClick={ e => setState(e.target.value)}>{newState.lucky[n]}</div>
 
       {/* <script src="js/main.js"></script> */}
     </div>
